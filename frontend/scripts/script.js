2f9e92f52c4b088b6a74a0e0f091d7634d4a3925
Saransh Kumar <cryptosingh@gmail.com>
2021-06-17 19:16:28 +0530

    update ui

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,24 +1,8 @@
 'use strict';
 
 document.getElementById('url').focus();
-
-const copy = document.getElementById('copy-box');
-copy.addEventListener('click', (e) => {
-    const label = document.getElementById('response');
-    navigator.clipboard.writeText(label.innerText);
-    alert('URL copied!');
-});
-
-function checkURL(url) {
-    const pattern = new RegExp('^htt(p|ps)://');
-    return pattern.test(url);
-}
-
-function getInputField() {
-    return document.getElementById('url');
-}
-
 const modal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+const toast = new bootstrap.Toast(document.getElementById('copy-toast'), { animation: true, autohide: true, delay: 2500 });
 
 document.getElementById('submitButton').addEventListener('click', async function (e) {
     const spinner = document.getElementById('spinner');
@@ -26,20 +10,48 @@ document.getElementById('submitButton').addEventListener('click', async function
     const label = document.getElementById('response');
     const div = document.getElementById('label-div');
     if (input.value === '') {
-        document.getElementById('error').innerHTML = 'Please enter a valid URL!';
-        modal.toggle();
+        showError('Please enter a valid URL!');
     } else {
-        const value = input.value;
-        const isValid = checkURL(value);
-        if (isValid) {
+        const url = input.value;
+        const reqUrl = `https://minify-url-spring-boot.herokuapp.com/api?url=${url}`;
+        if (isValid(url)) {
             spinner.classList.toggle('visually-hidden');
-            const response = await (await fetch(`https://minify-url-1.herokuapp.com/?url=${value}`, { method: 'POST' })).json();
-            spinner.classList.toggle('visually-hidden');
-            label.innerHTML = `<a href="${response.short_url}">${response.short_url}</a>`;
-            div.classList.remove('visually-hidden');
+            const res = await fetch(reqUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }).then(async (res) => {
+                res = await res.json();
+                spinner.classList.toggle('visually-hidden');
+                label.innerHTML = `<a href="${res.shortUrl}">${res.shortUrl}</a>`;
+                div.classList.remove('visually-hidden');
+            });
         } else {
             document.getElementById('error').innerHTML = 'Please enter the url<br>Like: https://www.google.com';
             modal.toggle();
         }
     }
+});
+
+function showError(message) {
+    document.getElementById('error').innerHTML = message;
+    modal.toggle();
+}
+
+function isValid(url) {
+    const pattern = new RegExp('^htt(p|ps)://www[.].*');
+    return pattern.test(url);
+}
+
+function getInputField() {
+    return document.getElementById('url');
+}
+
+const copy = document.getElementById('copy-box');
+copy.addEventListener('click', (e) => {
+    const label = document.getElementById('response');
+    navigator.clipboard.writeText(label.innerText);
+    toast.show();
 });
